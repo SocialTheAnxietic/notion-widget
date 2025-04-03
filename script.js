b37dc6624ad61779587fd8b6f1410b28737d0d7f
SocialTheAnxietic <127796245+SocialTheAnxietic@users.noreply.github.com>
2025-04-03 17:40:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ $(function () {
     "Proxy (Original Mix)"
   ];
   const trackNames = [
-    "Chappell Roan - ",
+    "Chappell Roan - The Giver",
     "Skylike - Dawn",
     "Kaaze - Electro Boy",
     "Jordan Schor - Home",
